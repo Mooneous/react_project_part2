8f31ec854787d92d074995c5f2cfb0e27d79eecb
Mooneous <4087022@naver.com>
2022-08-22 02:25:49 +0900

    메인페이지 News컴포넌트에서 Posts 3개까지 미리보기 출력

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,5 +1,28 @@
+import { useEffect, useState } from 'react';
+
 function News() {
-	return <section id='news'>News</section>;
+	const [Posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		let data = localStorage.getItem('post');
+		data = JSON.parse(data);
+		setPosts(data);
+	}, []); //처음 마운트 될때만 실행
+
+	return (
+		<section id='news'>
+			<h1>News</h1>
+			{Posts.map((post, idx) => {
+				if (idx >= 3) return; //post 3개까지만 보여주고 끝~
+				return (
+					<article key={idx}>
+						<h2>{post.title}</h2>
+						<p>{post.content}</p>
+					</article>
+				);
+			})}
+		</section>
+	);
 }
 
 export default News;

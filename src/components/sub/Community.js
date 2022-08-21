64b419faf8b5b9d99193e100566e46a8d877f1cf
Mooneous <4087022@naver.com>
2022-08-21 20:20:02 +0900

    글입력 양식 생성

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,7 +1,18 @@
 import Layout from '../common/Layout';
 
 function Community() {
-	return <Layout name={'Community'}>Community</Layout>;
+	return (
+		<Layout name={'Community'}>
+			<div className='inputBox'>
+				<input type='text' placeholdler='제목을 입력하세요' />
+				<br />
+				<textarea cols='30' rows='3' placeholder='본문을 입력하세요'></textarea>
+				<br />
+				<button>CANCEL</button>
+				<button>WRITE</button>
+			</div>
+		</Layout>
+	);
 }
 
 export default Community;

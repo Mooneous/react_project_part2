3e8419bb326da664801c462d08b6fc29ce7189fe
Mooneous <4087022@naver.com>
2022-08-22 17:16:58 +0900

    세로버튼 클릭시 스크롤 이동

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -1,10 +1,12 @@
-function Btns() {
+function Btns({ setIndex }) {
+	//4
 	return (
 		<ul className='scroll_navi'>
-			<li className='on'></li>
-			<li></li>
-			<li></li>
-			<li></li>
+			<li className='on' onClick={() => setIndex(0)}></li>
+			{/*//5*/}
+			<li onClick={() => setIndex(1)}></li>
+			<li onClick={() => setIndex(2)}></li>
+			<li onClick={() => setIndex(3)}></li>
 		</ul>
 	);
 }
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,15 +5,16 @@ import Pics from './Pics';
 import Vids from './Vids';
 import Btns from './Btns';
 
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import Anime from '../../assets/Anime';
 
 function Main() {
 	const main = useRef(null);
 	const pos = useRef([]);
+	const [Index, setIndex] = useState(0); //1
 
 	const getPos = () => {
 		pos.current = []; //기존배열(해당 섹션 위치값)값 초기화(4개씩만담김)
-
 		const secs = main.current.querySelectorAll('.myScroll');
 		for (const sec of secs) pos.current.push(sec.offsetTop); //push:값담기
 		console.log(pos.current);
@@ -22,11 +23,21 @@ function Main() {
 	useEffect(() => {
 		getPos();
 		window.addEventListener('resize', getPos);
-
 		return () => {
 			window.removeEventListener('resize', getPos);
 		};
 	}, []);
+
+	useEffect(() => {
+		//2
+		//console.log(index);//6. 세로버튼클릭하면콘솔에0,1,2,3찍힘
+		new Anime(window, {
+			//7
+			prop: 'scroll',
+			value: pos.current[Index],
+			duration: 500,
+		});
+	}, [Index]);
 
 	return (
 		<main ref={main}>
@@ -35,7 +46,8 @@ function Main() {
 			<News />
 			<Pics />
 			<Vids />
-			<Btns />
+			<Btns setIndex={setIndex} />
+			//3
 		</main>
 	);
 }

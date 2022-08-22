c999e135f9dedb31990fdfff3e5af8bf4a62d6be
Mooneous <4087022@naver.com>
2022-08-22 16:51:13 +0900

    브라우저 리사이즈시 해당 섹션위치값을 useRef로 생성한 참조객체에 저장

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -8,9 +8,24 @@ import { useRef, useEffect } from 'react';
 
 function Main() {
 	const main = useRef(null);
-
-	useEffect(() => {
+	const pos = useRef([]);
+	//3.
+	const getPos = () => {
+		//4.
+		pos.current = []; //기존배열(해당 섹션 위치값)값 초기화(4개씩만담김)
+		//1.
 		const secs = main.current.querySelectorAll('.myScroll');
+		for (const sec of secs) pos.current.push(sec.offsetTop); //push:값담기
+		console.log(pos.current);
+	};
+	//2.
+	useEffect(() => {
+		getPos();
+		window.addEventListener('resize', getPos);
+
+		return () => {
+			window.removeEventListener('resize', getPos);
+		};
 	}, []);
 
 	return (

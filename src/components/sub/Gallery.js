610782fbc95431ec8f6fe143545cd0c2bcf1496a
Mooneous <4087022@naver.com>
2022-08-23 02:11:04 +0900

    interest갤러리 출력 및 스타일링(float이용)

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -5,50 +5,50 @@ import { useEffect, useState } from 'react';
 
 function Gallery() {
 	const key = 'e3e1c697d136946fb4d52bffc2cc35b7';
-	const method = 'flickr.people.getPhotos';
-	const num = 10;
+	const method_interest = 'flickr.interestingness.getList';
+	const num = 20;
 	const id = '164021883@N04';
-	const url = `https://www.flickr.com/services/rest/?method=${method}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1&user_id=${id}`;
+	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
 
 	const [Items, setItems] = useState([]);
-	const [Index, setIndex] = useState(0); //썸네일클릭시 인덱스를 가져와서 그에맞는 이미지 팝업하기 위해 필요
+	const [Index, setIndex] = useState(0);
 	const [Open, setOpen] = useState(false);
 
 	useEffect(() => {
 		axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
-			setItems(json.data.photos.photo); //옮겨담기
+			setItems(json.data.photos.photo);
 		});
 	}, []);
 
 	return (
 		<>
 			<Layout name={'Gallery'}>
-				{Items.map((pic, idx) => {
-					return (
-						//article클릭했을때 setIndex값을 현재 idx값으로, setOpem을 true로 변경하기~
-						<article
-							key={idx}
-							onClick={() => {
-								setIndex(idx);
-								setOpen(true);
-							}}>
-							<div className='pic'>
-								<img
-									src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-									alt={pic.title}
-								/>
-							</div>
-
-							<h2>{pic.title}</h2>
-						</article>
-					);
-				})}
+				<div className='frame on'>
+					{Items.map((pic, idx) => {
+						return (
+							<article
+								key={idx}
+								onClick={() => {
+									setIndex(idx);
+									setOpen(true);
+								}}>
+								<div className='inner'>
+									<div className='pic'>
+										<img
+											src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+											alt={pic.title}
+										/>
+									</div>
+									<h2>{pic.title}</h2>
+								</div>
+							</article>
+						);
+					})}
+				</div>
 			</Layout>
-			{/*오픈 값이 참일때 실행 */}
 			{Open && (
 				<Popup setOpen={setOpen}>
-					{/*Items라는 state데이터의 Index번째의~~가져오기*/}
 					<img
 						src={`https://live.staticflickr.com/${Items[Index].server}/${Items[Index].id}_${Items[Index].secret}_b.jpg`}
 						alt={Items[Index].title}

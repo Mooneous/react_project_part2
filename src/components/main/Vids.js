b1c3315d5a146ab0b25f3c4f1e5e17c79734b2f5
Mooneous <4087022@naver.com>
2022-08-22 22:55:24 +0900

    pagination과 다양한 옵션 추가(#vids섹션)

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,12 +1,20 @@
 //swiper연결 : npm i swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
 import 'swiper/css';
+import 'swiper/css/pagination';
 
 function Vids() {
 	return (
 		<section id='vids' className='myScroll'>
 			<h1>Recent Youtube</h1>
-			<Swiper>
+			<Swiper
+				pagination={{ clickable: true }}
+				modules={[Pagination]}
+				spaceBetween={50}
+				loop={true}
+				slidesPerView={3}
+				centeredSlides={true}>
 				<SwiperSlide>1</SwiperSlide>
 				<SwiperSlide>2</SwiperSlide>
 				<SwiperSlide>3</SwiperSlide>
@@ -18,3 +26,7 @@ function Vids() {
 }
 
 export default Vids;
+// clickable : 아래 점점점 클릭
+// spaceBetween : 슬라이드 사이 간격
+// slidesPerView : 한 화면에 보일 패널 개수
+// centeredSlides : 1번이 가운데로

8b6f501909cc83c8ccede8ed101c16b347063465
Mooneous <4087022@naver.com>
2022-08-23 02:45:27 +0900

    코드 동기화(순서대로)(2.async await)시키고 이후 frame요소(갤러리이미지들) 출력

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,23 +1,27 @@
 import Layout from '../common/Layout';
 import Popup from '../common/Popup';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 function Gallery() {
+	//3. frame 참조객체 생성
+	const frame = useRef(null);
 	const [Items, setItems] = useState([]);
 	const [Index, setIndex] = useState(0);
 	const [Open, setOpen] = useState(false);
-
-	const getFlickr = () => {
+	//2. async await로 동기화
+	const getFlickr = async () => {
 		const key = 'e3e1c697d136946fb4d52bffc2cc35b7';
 		const method_interest = 'flickr.interestingness.getList';
 		const num = 20;
 		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&format=json&nojsoncallback=1`;
 
-		axios.get(url).then((json) => {
+		await axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
 			setItems(json.data.photos.photo);
 		});
+		//5. 이미지데이터를 모두 불러온 후 현재frame에 클래스 on 추가(6. 하면 이미지들 아래서 위로 올라옴 - css효과)
+		frame.current.classList.add('on');
 	};
 
 	useEffect(getFlickr, []);
@@ -25,7 +29,8 @@ function Gallery() {
 	return (
 		<>
 			<Layout name={'Gallery'}>
-				<div className='frame on'>
+				{/*1. class.on지우기 4.ref로 참조*/}
+				<div className='frame' ref={frame}>
 					{Items.map((pic, idx) => {
 						return (
 							<article

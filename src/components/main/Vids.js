53dfac4ff97050f4a8f3b424cf85f7fba188a9c5
Mooneous <4087022@naver.com>
2022-08-22 23:17:14 +0900

    불렛,버튼스타일링(#vids섹션)

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,16 +1,18 @@
 //swiper연결 : npm i swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
+import { Pagination, Navigation } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 function Vids() {
 	return (
 		<section id='vids' className='myScroll'>
 			<h1>Recent Youtube</h1>
 			<Swiper
+				modules={[Pagination, Navigation]}
 				pagination={{ clickable: true }}
-				modules={[Pagination]}
+				navigation={true}
 				spaceBetween={50}
 				loop={true}
 				slidesPerView={3}
@@ -40,3 +42,4 @@ export default Vids;
 // spaceBetween : 슬라이드 사이 간격
 // slidesPerView : 한 화면에 보일 패널 개수
 // centeredSlides : 1번이 가운데로
+// navigation : 좌우이동 화살표 - 모듈이 가장 위에 있어야 작동함

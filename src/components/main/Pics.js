a309df83d91f8a781e0fbcd547a4f010a589deb8
Mooneous <4087022@naver.com>
2022-08-27 18:05:31 +0900

    Main페이지Pics아티클에 Flickr미리보기 데이터 출력

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -1,4 +1,8 @@
+import { useSelector } from 'react-redux';
+
 function Pics({ Scrolled, currentPos }) {
+	const Pics = useSelector((store) => store.flickr.data);
+
 	const position = Scrolled - currentPos || 0;
 	//console.log(position);
 	return (
@@ -22,6 +26,20 @@ function Pics({ Scrolled, currentPos }) {
 					transform: `scale(${1 + position / 1000})`,
 					opacity: 1 - position / 1000,
 				}}></div>
+
+			<ul>
+				{Pics.map((pic, idx) => {
+					if (idx >= 5) return;
+					return (
+						<li key={pic.id}>
+							<img
+								src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+								alt={pic.title}
+							/>
+						</li>
+					);
+				})}
+			</ul>
 		</section>
 	);
 }
